e9ac6fac28e936b0a6b818c1699d147a6ca8c257
Vitor Valente <dev.vitorlucas@gmail.com>
2025-03-17 22:17:18 -0300

    feat: create the Models folder and add the DatabaseConfig class

---
+++ Models/DatabaseConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Fix.Models
+{
+    public class DatabaseConfig
+    {
+        public string DataSource { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public int Timeout { get; set; }
+    }
+}
